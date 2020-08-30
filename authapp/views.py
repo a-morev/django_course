9154ea3b0f540284a66d31257685416f667dc92f
a-morev <ion05pk@gmail.com>
2020-08-31 01:45:55 +0300

    les_4:  - created authapp  - created model ShopClient  - create fill_db.py  - created login form

--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,30 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from authapp.forms import ShopLoginForm
 
 
 def login(request):
-    pass
+    form = None
+    if request.method == 'POST':
+        form = ShopLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    elif request.method == 'GET':
+        form = ShopLoginForm()
+
+    context = {
+        'page_title': 'аутентификация',
+        'form': form,
+    }
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
